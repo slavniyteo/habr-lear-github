a3bff95d384133386d0abbbdf211c05627b5c1d1
slavniyteo <slavniyteo@gmail.com>
2020-08-24 09:05:12 +0300

    run.sh: final version (to copy to the article)

--- run.sh
+++ run.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
+# Скрипт прервется, если встретит непроинициализированную переменную
+# либо какая-то функция/команда вернет код ответа, отличный от нуля
 set -eu
 
-################################################################################
+########## Подготавливаем окружение ############################################
 
-export GIT_DIR="./poetry/.git"
-export GIT_WORK_TREE="./poetry"
 FILE="${1:-./poem.txt}"
-OUT_FILE="./poetry/README.md"
+OUT_DIR="${2:-./poetry}"
+OUT_FILE="${OUT_DIR}/README.md"
 
-################################################################################
+export GIT_DIR="${OUT_DIR}/.git"
+export GIT_WORK_TREE="$OUT_DIR"
+
+if [[ -d "$OUT_DIR" ]]; then
+    echo "Directory ${OUT_DIR} exists already" >&2
+    exit 1
+fi
+
+if [[ ! -s "$FILE" ]]; then
+    echo "File ${FILE} doesn't exist or empty" >&2
+    exit 2
+fi
+
+mkdir -p "$OUT_DIR"
+touch "$OUT_FILE"
+
+git init 
+
+# Убеждаемся, что гит не будет подписывать коммиты
+git config --local commit.gpgsign no
+
+########## Список будущих контрибьюторов #######################################
 
 declare -A AUTHORS=( )
 AUTHORS["Linus Torvalds"]=torvalds@linux-foundation.org
@@ -17,40 +39,53 @@ AUTHORS["Vitalik Buterin"]=v@buterin.com
 AUTHORS["Bjarne Stroustrup"]=bjarne@stroustrup.com
 AUTHORS["Brendan Gregg"]=brendan.d.gregg@gmail.com
 AUTHORS["Guido van Rossum"]=guido@python.org
+AUTHORS["Aleksey Boomburum"]=boomburum@gmail.com
 AUTHORS["Ivan Vasilev"]=slavniyteo@gmail.com
 
+# Массив имен разработчиков, чтобы идти по ним в цикле
+# В функции get-next-author-name
 declare -a AUTHOR_NAMES=( "${!AUTHORS[@]}" )
 
+########## Функции #############################################################
+
 get-next-author-name() {
-  GLOBAL_AUTHOR_INDEX="$1"
-  AUTHOR_INDEX="$(($GLOBAL_AUTHOR_INDEX % "${#AUTHOR_NAMES[@]}"))"
-  echo "${AUTHOR_NAMES["$AUTHOR_INDEX"]}"
+    local GLOBAL_AUTHOR_INDEX="$1"
+    local AUTHOR_INDEX="$(($GLOBAL_AUTHOR_INDEX % "${#AUTHOR_NAMES[@]}"))"
+
+    echo "${AUTHOR_NAMES["$AUTHOR_INDEX"]}"
 }
 
-commit () {
-  local AUTHOR="$1"
-  local LINE="$2"
-  local AUTHOR_EMAIL="${AUTHORS["$AUTHOR"]}"
+commit-one-line() {
+    local AUTHOR="$1"
+    local LINE="$2"
+    local AUTHOR_EMAIL="${AUTHORS["$AUTHOR"]}"
 
-  echo "$LINE" | cat - "$OUT_FILE" > temp
-  mv temp "$OUT_FILE"
+    # Вставляем строку в начало файла
+    echo "$LINE" | cat - "$OUT_FILE" > temp
+    mv temp "$OUT_FILE"
 
-  git add -A
+    git add -A
 
-  git config --local user.name "$AUTHOR"
-  git config --local user.email "$AUTHOR_EMAIL"
-  git commit -m "| $LINE"
-  git config --local --unset user.name
-  git config --local --unset user.email
+    git config --local user.name "$AUTHOR"
+    git config --local user.email "$AUTHOR_EMAIL"
+
+    # Добавляем '| ' в начало, 
+    # чтобы избежать проблем с пустым сообщением коммита
+    git commit -m "| $LINE"
+
+    # Избавляемся от сайд-эффектов функции
+    git config --local --unset user.name
+    git config --local --unset user.email
 }
 
-################################################################################
+########## Основной цикл #######################################################
 
-GLOBAL_AUTHOR_INDEX=0
+LINE_IDX=0
 while IFS= read LINE; do
-  GLOBAL_AUTHOR_INDEX="$((GLOBAL_AUTHOR_INDEX + 1))"
-  AUTHOR="$(get-next-author-name "$GLOBAL_AUTHOR_INDEX")"
-  commit "$AUTHOR" "$LINE"
-done <<<"$(tac "$FILE")"
+    LINE_IDX="$((LINE_IDX + 1))"
+    AUTHOR="$(get-next-author-name "$LINE_IDX")"
+
+    commit-one-line "$AUTHOR" "$LINE"
+done <<<"$(tac "$FILE")" # Читаем файл от последней строки к первой
 
 ################################################################################
